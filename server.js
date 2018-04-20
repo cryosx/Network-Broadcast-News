85c8e6d733957e69e11b37cd3cae95a0db600f5b
cryosx <cyruswu.email@gmail.com>
2018-04-19 16:47:27 -1000

    Switched from an Array to an Object for connections

--- server.js
+++ server.js
@@ -1,22 +1,39 @@
 const net = require('net');
-
-let connections = [];
+let connections = {};
+// let connection2 = {};
 
 const server = net.createServer(c => {
   // 'connection' listener
+  connections[c.remotePort] = {
+    socket: c,
+    address: c.remoteAddress,
+    port: c.remotePort
+  };
 
-  connections.push(c);
   c.pipe(process.stdout);
 
-  console.log('client connected');
+  console.log(`CONNECTED: ${c.remoteAddress}:${c.remotePort}`);
 
   c.on('data', data => {
-    connections.forEach(function(connection) {
-      connection.write(`Echo: ${data.toString()}`);
-    });
+    for (const connection in connections) {
+      if (connections.hasOwnProperty(connection)) {
+        if (c.remotePort != connection) {
+          connections[connection].socket.write(
+            `MESSAGE FROM ${c.remoteAddress}:${
+              c.remotePort
+            }: ${data.toString()}`
+          );
+        }
+      }
+    }
+    // console.log(connections[c.remotePort]);
+    // connections.forEach(function(connection) {
+    //   connection.write(`: ${data.toString()}`);
+    // });
   });
   c.on('close', () => {
-    connections.splice(connections.indexOf(c), 1);
+    // connections.splice(connections.indexOf(c), 1);
+    delete connections[c.remotePort];
   });
   c.on('end', () => {
     console.log('client disconnected');
@@ -32,8 +49,7 @@ server.listen(
     port: 6969
   },
   () => {
-    console.log('Server listening on 0.0.0.0:6969');
-    // console.log(server);
+    console.log(`Server listening on ${server._connectionKey.substring(2)}`);
   }
 );
 
@@ -42,9 +58,11 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    connections.forEach(function(connection) {
-      connection.write(`BROADCAST: ${chunk}`);
-    });
+    for (const connection in connections) {
+      if (connections.hasOwnProperty(connection)) {
+        connections[connection].socket.write(`SERVER BROADCAST: ${chunk}`);
+      }
+    }
   }
 });
 
