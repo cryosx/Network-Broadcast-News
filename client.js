ebeecca4c691c997e901fc8972923bef7118eafd
cryosx <cyruswu.email@gmail.com>
2018-04-20 09:38:14 -1000

    kick functionality added

--- client.js
+++ client.js
@@ -1,31 +1,67 @@
 const net = require('net');
 
-const client = net.createConnection(
+let username = null;
+
+const serverConnection = net.createConnection(
   {
     host: '0.0.0.0',
     port: 6969
   },
   () => {
-    //'connect' listener
-    console.log('connected to server!');
+    //'connect' to listener
+    // process.stdin.pipe(serverConnection);
+    console.log('Connected to server!');
+    // serverConnection.emit('prompt_username');
   }
 );
-client.on('data', data => {
-  console.log(`${data.toString()}`);
+
+// serverConnection.once('connection', function() {});
+serverConnection.on('prompt_username', function() {
+  console.log('Enter a USERNAME:');
 });
-client.on('end', () => {
+
+serverConnection.on('username', function(data) {
+  if (data !== null) {
+    username = data;
+    serverConnection.write(`USERNAME:${data}`);
+  }
+});
+serverConnection.setEncoding('utf8');
+serverConnection.on('data', function(data) {
+  let index = data.indexOf(':');
+  let header = data.substring(0, index);
+  let msg = data.substring(index + 1);
+  if (header === 'USERNAME_TAKEN') {
+    console.log(msg);
+    username = null;
+    serverConnection.emit('prompt_username');
+  }
+  if (header === 'KICK') {
+    console.log(`${msg}`);
+  }
+
+  if (header === 'MESSAGE') {
+    console.log(`${msg}`);
+  }
+  // serverConnection.pipe(process.stdout);
+});
+serverConnection.on('end', () => {
   console.log('disconnected from server');
 });
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('readable', () => {
+process.stdin.on('readable', data => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    client.write(`${chunk}`);
+    if (!username) {
+      serverConnection.emit('username', chunk.trim());
+    } else {
+      serverConnection.write(`MESSAGE:${chunk.trim()}`);
+    }
   }
 });
 
-process.stdin.on('end', () => {
+process.stdin.on('end', function() {
   process.stdout.write('end');
 });
--- server.js
+++ server.js
@@ -1,48 +1,74 @@
 const net = require('net');
-let connections = {};
-// let connection2 = {};
 
-const server = net.createServer(c => {
+let connections = {};
+let usernames = ['[ADMIN]'];
+
+const server = net.createServer(function(c) {
+  c.setEncoding('utf8');
+
   // 'connection' listener
+  // connection keys will be using ports
   connections[c.remotePort] = {
-    socket: c,
-    address: c.remoteAddress,
-    port: c.remotePort
+    client: c,
+    username: null
   };
 
-  c.pipe(process.stdout);
+  // c.pipe(process.stdout);
 
+  c.write(`MESSAGE:Enter a USERNAME:`);
   console.log(`CONNECTED: ${c.remoteAddress}:${c.remotePort}`);
 
-  c.on('data', data => {
-    for (const connection in connections) {
-      if (connections.hasOwnProperty(connection)) {
-        if (c.remotePort != connection) {
-          connections[connection].socket.write(
-            `MESSAGE FROM ${c.remoteAddress}:${
-              c.remotePort
-            }: ${data.toString()}`
-          );
+  // c.on('username', function() {});
+  c.on('data', function(data) {
+    data = data.toString().trim();
+    let index = data.indexOf(':');
+    let header = data.substring(0, index);
+    let msg = data.substring(index + 1);
+    if (header === 'USERNAME') {
+      if (usernames.includes(msg)) {
+        if (msg === '[ADMIN]') {
+          c.write(`USERNAME_TAKEN:Nice try buddy, but you're not an admin.`);
+        } else {
+          c.write(`USERNAME_TAKEN:Username taken, enter a differnt one.`);
+        }
+      } else {
+        connections[c.remotePort].username = msg;
+        usernames.push(msg);
+        console.log(usernames);
+      }
+    } else if (header === 'MESSAGE') {
+      if (connections[c.remotePort].username === null) {
+        c.write(`MESSAGE:Bad client!`);
+        c.end();
+      } else {
+        for (const port in connections) {
+          if (connections.hasOwnProperty(port)) {
+            if (c.remotePort != port) {
+              let client = connections[port].client;
+              let username = connections[c.remotePort].username;
+              client.write(`MESSAGE:${username}: ${msg}`);
+            }
+          }
         }
       }
+    } else {
+      c.write(`MESSAGE:Bad client!`);
+      c.end();
     }
-    // console.log(connections[c.remotePort]);
-    // connections.forEach(function(connection) {
-    //   connection.write(`: ${data.toString()}`);
-    // });
   });
-  c.on('close', () => {
-    // connections.splice(connections.indexOf(c), 1);
-    delete connections[c.remotePort];
+  c.on('close', function() {
+    console.log(`CLOSED: ${c.remoteAddress}:${c.remotePort}`);
+    removeClient(c.remotePort);
   });
-  c.on('end', () => {
+  c.on('end', function() {
     console.log('client disconnected');
   });
 });
 
-server.on('error', err => {
-  throw err;
+server.on('error', function(error) {
+  throw error;
 });
+
 server.listen(
   {
     host: '0.0.0.0',
@@ -58,9 +84,32 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    for (const connection in connections) {
-      if (connections.hasOwnProperty(connection)) {
-        connections[connection].socket.write(`SERVER BROADCAST: ${chunk}`);
+    let arguments = chunk.trim().split(' ');
+
+    if (arguments[0] === `\\kick`) {
+      let index = usernames.indexOf(arguments[1]);
+      if (index !== -1) {
+        console.log(`Kicking ${[arguments[1]]}`);
+        // usernames.splice(index, 1);
+        for (const port in connections) {
+          if (connections.hasOwnProperty(port)) {
+            if (connections[port].username === arguments[1]) {
+              connections[port].client.write(
+                `KICK:You have been kicked from server by [ADMIN]`
+              );
+              connections[port].client.end();
+              // delete connections[port];
+              break;
+            }
+          }
+        }
+      }
+    } else {
+      for (const port in connections) {
+        let client = connections[port].client;
+        if (connections.hasOwnProperty(port)) {
+          client.write(`MESSAGE:[ADMIN] ${chunk.trim()}`);
+        }
       }
     }
   }
@@ -69,3 +118,10 @@ process.stdin.on('readable', () => {
 process.stdin.on('end', () => {
   process.stdout.write('end');
 });
+
+function removeClient(port) {
+  let username = connections[port].username;
+  let index = usernames.indexOf(username);
+  usernames.splice(index, 1);
+  delete connections[port];
+}
